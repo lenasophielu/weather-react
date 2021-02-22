b258678ff5204d5a35640e839a8d061ecdfb2f1e
lenasophielu <72163661+lenasophielu@users.noreply.github.com>
2021-02-23 00:28:42 +0100

    added function dateCity

---
+++ src/DateCity.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function DateCity() {
+  return (
+    <div class="row row-cols-3">
+      <div id="city" class="city"></div>
+      <div class="today">
+        <img src="" alt="Clear" id="icon" />
+      </div>
+      <div id="date" class="date"></div>
+      <div class="row"></div>
+    </div>
+  );
+}
