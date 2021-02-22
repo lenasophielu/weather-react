0f17312dd67634460515a3ac5b77741c6277aaea
lenasophielu <72163661+lenasophielu@users.noreply.github.com>
2021-02-23 00:30:29 +0100

    added function weather.js

--- src/weather.js
+++ src/weather.js
@@ -1,6 +1,22 @@
 import React from "react";
 import axios from 'axios';
 
-export default function Weather(){
-    return <h2>Hello from Weather</h2>;
+export default function Weather() {
+  return (
+    <div>
+      <div class="row row-cols-9">
+        <div id="temperature" class="temperature"></div>
+        <div class="unit">°C </div>
+      </div>
+      <span class="weatherDefinition"></span>
+      <ul class="conditions">
+        <li>
+          Humidity: <span id="humidity"></span>%
+        </li>
+        <li>
+          Wind: <span id="wind"></span> km/h
+        </li>
+      </ul>
+    </div>
+  );
 }
