93604f6cd5aa3d40a9155e3d4254610deb953f16
lenasophielu <72163661+lenasophielu@users.noreply.github.com>
2021-02-23 00:25:49 +0100

    start

--- src/App.js
+++ src/App.js
@@ -21,5 +21,69 @@ function App() {
     </div>
   );
 }
+import Weather from "./Weather";
+import Search from "./Search";
+import DateCity from "./DateCity";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div class="container">
+        <div class="weather-app">
+          <form class="search-form" id="search-form">
+            <div class="form-row">
+              <div class="col-5">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Where?"
+                  id="city-input"
+                />
+              </div>
+              <div class="col-1.5">
+                <input
+                  type="submit"
+                  value="Search"
+                  class="btn btn-info"
+                  id="searchbutton"
+                />
+              </div>
+
+              <button
+                type="button"
+                class="btn btn-secondary"
+                id="current-location-button"
+              >
+                Current
+              </button>
+            </div>{" "}
+          </form>
+        </div>
+
+        <div class="row row-cols-3">
+          <div id="city" class="city"></div>
+          <div class="today">
+            <img src="" alt="Clear" id="icon" />
+          </div>
+          <div id="date" class="date"></div>
+          <div class="row"></div>
+        </div>
+        <div class="row row-cols-9">
+          <div id="temperature" class="temperature"></div>
+          <div class="unit">°C </div>
+        </div>
+        <span class="weatherDefinition"></span>
+        <ul class="conditions">
+          <li>
+            Humidity: <span id="humidity"></span>%
+          </li>
+          <li>
+            Wind: <span id="wind"></span> km/h
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
 
 export default App;
