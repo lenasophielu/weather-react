ae415fddbc638473372efa72f944debc779fd361
lenasophielu <72163661+lenasophielu@users.noreply.github.com>
2021-02-23 13:39:57 +0100

    replaced app

--- src/App.js
+++ src/App.js
@@ -2,35 +2,7 @@ import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-import Weather from "./Weather";
-import Search from "./Search";
-import DateCity from "./DateCity";
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-import Weather from "./Weather";
-import Search from "./Search";
-import DateCity from "./DateCity";
-
-export default function App() {
   return (
     <div className="App">
       <div class="container">
